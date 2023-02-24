09798048c829c06aa8e7a9bc35f9416bb6c9ad39
Jodelin Desramaeaux <djodelin93@gmail.com>
2023-02-23 22:07:55 -0500

    initial setup for the project

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EmployeesModule } from './employees/employees.module';
+import { StudentsModule } from './students/students.module';
+import { TeachersModule } from './teachers/teachers.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { CommonModule } from './commons/common.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      logging: false,
+      autoLoadEntities: true
+    }),
+    EmployeesModule,
+    StudentsModule,TeachersModule,UsersModule, AuthModule,CommonModule 
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
